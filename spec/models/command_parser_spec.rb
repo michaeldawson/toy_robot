def2944258fc9429b056eb685b66eb788fbfe22a
Michael Dawson <email.michaeldawson@gmail.com>
2016-08-01 14:24:24 +1000

    Fix test whoopsie

--- spec/models/command_parser_spec.rb
+++ spec/models/command_parser_spec.rb
@@ -8,60 +8,62 @@ RSpec.describe CommandParser do
     allow(Robot).to receive(:new).and_return(robot)
   end
 
-  context 'with valid commands separated by newlines' do
-    let(:input) {
-      'PLACE 0,0,NORTH
-      MOVE
-      REPORT'
-    }
+  describe '#perform' do
+    context 'with valid commands separated by newlines' do
+      let(:input) {
+        'PLACE 0,0,NORTH
+        MOVE
+        REPORT'
+      }
 
-    it 'dispatches commands' do
-      expect(robot).to receive(:place).with(0, 0, 'NORTH')
-      expect(robot).to receive(:move)
-      expect(robot).to receive(:report)
-      command_parser.perform
-    end
-  end
-
-  context 'with valid commands separated by newlines, with some extra spacing' do
-    let(:input) {
-      'PLACE 0, 0, NORTH'
-    }
-
-    it 'dispatches commands' do
-      expect(robot).to receive(:place).with(0, 0, 'NORTH')
-      command_parser.perform
-    end
-  end
-
-  context 'with invalid commands and valid commands' do
-    let(:input) {
-      'CHICKEN 0,0,NORTH
-      DESTROY
-      RM -RF /
-      PLACE 0,0,NORTH'
-    }
-
-    it "doesn't dispatch invalid commands, but dispatches the valid commands" do
-      expect(robot).not_to receive(:chicken)
-      expect(robot).not_to receive(:destroy)
-      expect(robot).not_to receive(:rm)
-      expect(robot).to receive(:place)
-      command_parser.perform
-    end
-  end
-
-  context 'with invalid commands with whitespace' do
-    let(:input) {
-      '1234
-
-      '
-    }
-
-    it "doesn't raise an error" do
-      expect {
+      it 'dispatches commands' do
+        expect(robot).to receive(:place).with(0, 0, 'NORTH')
+        expect(robot).to receive(:move)
+        expect(robot).to receive(:report)
         command_parser.perform
-      }.not_to raise_error
+      end
+    end
+
+    context 'with valid commands separated by newlines, with some extra spacing' do
+      let(:input) {
+        'PLACE 0, 0, NORTH'
+      }
+
+      it 'dispatches commands' do
+        expect(robot).to receive(:place).with(0, 0, 'NORTH')
+        command_parser.perform
+      end
+    end
+
+    context 'with invalid commands and valid commands' do
+      let(:input) {
+        'CHICKEN 0,0,NORTH
+        DESTROY
+        RM -RF /
+        PLACE 0,0,NORTH'
+      }
+
+      it "doesn't dispatch invalid commands, but dispatches the valid commands" do
+        expect(robot).not_to receive(:chicken)
+        expect(robot).not_to receive(:destroy)
+        expect(robot).not_to receive(:rm)
+        expect(robot).to receive(:place)
+        command_parser.perform
+      end
+    end
+
+    context 'with invalid commands with whitespace' do
+      let(:input) {
+        '1234
+
+        '
+      }
+
+      it "doesn't raise an error" do
+        expect {
+          command_parser.perform
+        }.not_to raise_error
+      end
     end
   end
 end
